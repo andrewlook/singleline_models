31b70a6f2855219fecb2b97db1317c206021d687
Andrew Look <look@aesthetic.com>
2024-02-25 16:46:01 -0500

    wip

--- sketch_transformer/__init__.py
+++ sketch_transformer/__init__.py
@@ -2,7 +2,7 @@ from .config import *
 from .dataset import *
 from .layers import *
 from .masks import *
-from .model import *
+# from .model import *
 from .sampler import *
-from .trainer import *
+# from .trainer import *
 from .utils import *
