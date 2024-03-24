wip: fix off-by-one sequence issue for inp/tar_inp in training loop. fix metadata loss

singleline_models/sketch_transformer/layers.py
@@ -74,10 +74,22 @@ class DecoderLayer(nn.Module):
         enc_output will have shape `[batch_size, input_seq_len, d_model]`
         """
 
+        # print(f"\t\t\tDecoderLayer.forward: (MHA1 K,Q,V)     x={x.shape} ")
+        # print(f"\t\t\tDecoderLayer.forward: (MHA1 mask)      dec_target_padding_mask={dec_target_padding_mask.shape}")
+        # print(f"\t\t\tDecoderLayer.forward: (MHA1 lookahead) look_ahead_mask={look_ahead_mask.shape}")
+
         attn1, attn1_weights = self.mha1(x, x, x, key_padding_mask=dec_target_padding_mask, attn_mask=look_ahead_mask)
         out1 = self.ln1(x + attn1)
 
-        attn2, attn2_weights = self.mha2(enc_output, enc_output, out1, padding_mask)
+        # print(f"\t\t\tDecoderLayer.forward: (MHA2 K,Q)  enc_output={enc_output.shape}")
+        # print(f"\t\t\tDecoderLayer.forward: (MHA2 V)    out1={out1.shape}")
+        # print(f"\t\t\tDecoderLayer.forward: (MAH2 mask) padding_mask={padding_mask.shape}")
+
+
+        x2 = enc_output[:, :out1.shape[1], ...]
+        # print(f"\t\t\tDecoderLayer.forward: (MHA2 K,Q)  x2={x2.shape}   (CHOPPED)")
+        
+        attn2, attn2_weights = self.mha2(x2, x2, out1, dec_target_padding_mask) #padding_mask)
         out2 = self.ln2(out1 + attn2)
 
         ffn_output = self.ffn(out2)

singleline_models/sketch_transformer/masks.py
@@ -26,8 +26,6 @@ def create_lookahead_mask(seq_len):
 
 
 def create_masks(input_seq, target_seq, device='cuda'):
-
-    # Encoder padding mask
     enc_padding_mask = create_padding_mask(input_seq)
 
     # Used in the 2nd attention block in the decoder.
@@ -40,16 +38,8 @@ def create_masks(input_seq, target_seq, device='cuda'):
     look_ahead_mask = create_lookahead_mask(target_seq.shape[1])
     dec_target_padding_mask = create_padding_mask(target_seq)
 
-    # print(target_seq.shape, look_ahead_mask.shape, dec_target_padding_mask.shape)
-
     # NOTE: torch nn.MHA takes separate padding & attn masks w/ different shapes,
-    #       so use that instead of combining here. TODO: check the source for how
-    #       they combine.
-    #
-    # # TODO: WTF is combined_mask used for???
-    # # TODO: can I verify this...?
-    # combined_mask = torch.fmax(look_ahead_mask, dec_target_padding_mask.unsqueeze(1))
-
+    #       so use that instead of combining here
     return enc_padding_mask.to(device), dec_padding_mask.to(device), dec_target_padding_mask.to(device), look_ahead_mask.to(device)
 
 

singleline_models/sketch_transformer/model.py
@@ -61,6 +61,13 @@ class Decoder(nn.Module):
         # add positional embedding, and apply dropout
         x = self.pos_encoding(x)
 
+        # print(f"\t\tDecoder.forward: x={x.shape}")
+        # print(f"\t\tDecoder.forward: enc_output={enc_output.shape}")
+
+        # print(f"\t\tDecoder.forward: padding_mask={padding_mask.shape}")
+        # print(f"\t\tDecoder.forward: dec_target_padding_mask={dec_target_padding_mask.shape}")
+        # print(f"\t\tDecoder.forward: look_ahead_mask={look_ahead_mask.shape}")
+
         attention_weights = {}
         for i, layer in enumerate(self.dec_layers):
             x, block1, block2 = layer(x, enc_output, padding_mask, dec_target_padding_mask, look_ahead_mask)
@@ -76,14 +83,26 @@ class ReconstructionLoss(nn.Module):
    
     def forward(self, pred, real):
         pred_locations = pred[:, :, :2]
-        # pred_metadata = pred[:, :, 2:]
-        tgt_locations = real[:, :, :2]
-        # tgt_metadata = real[:, :, 2:]
-        location_loss = F.mse_loss(pred_locations, tgt_locations) #, reduction='none')
-        return location_loss
+        real_locations = real[:, :, :2]
+        location_loss = F.mse_loss(pred_locations, real_locations)
+
+        pred_metadata = pred[:, :, 2:] # un-normalized logits
+        real_metadata = real[:, :, 2:] # true labels in probability spaces (add up to 1).
+
+        # for K-dimensional inputs, torch cross_entropy() requires:
+        # - batch as first dimension,
+        # - class as second,
+        # - ... other dimensions
+        pred_metadata = pred_metadata.transpose(2, 1) # [batch_size, n_classes, n_seq]
+        real_idx = torch.argmax(real_metadata.transpose(2, 1), dim=1) # [batch_size, n_seq]
+
+        metadata_loss = F.cross_entropy(pred_metadata, real_idx)
+
+        loss = location_loss + metadata_loss
+
+        # import ipdb; ipdb.set_trace()
         
-        # metadata_loss = F.cross_entropy(F.softmax(pred_metadata, dim=-1), F.softmax(tgt_metadata, dim=-1)) #, reduction='none')
-        # return location_loss, metadata_loss
+        return loss
 
 # %% ../../nbs/sketch_transformer/02_model.ipynb 8
 class Model(nn.Module):
@@ -116,13 +135,36 @@ class Model(nn.Module):
         """Generate logits"""
         padding_mask = torch.zeros_like(dec_padding_mask) if self.hp.blind_decoder_mask else dec_padding_mask
         pre_decoder = self.expand_layer(embedding)
-        dec_output, attention_weights = self.decoder(target, pre_decoder, padding_mask, dec_target_padding_mask, look_ahead_mask)
+
+        # print(f"\tModel.decode: embedding={embedding.shape}")
+        # print(f"\tModel.decode: pre_decoder={pre_decoder.shape}")
+
+        # print(f"\tModel.decode: target={target.shape}")
+        
+        # print(f"\tModel.decode: dec_padding_mask={padding_mask.shape}")
+        # print(f"\tModel.decode: dec_target_padding_mask={dec_target_padding_mask.shape}")
+        # print(f"\tModel.decode: look_ahead_mask={look_ahead_mask.shape}")
+        
+        dec_output, attention_weights = self.decoder(
+            x=target,
+            enc_output=pre_decoder,
+            padding_mask=padding_mask,
+            dec_target_padding_mask=dec_target_padding_mask,
+            look_ahead_mask=look_ahead_mask)
         final_output = self.output_layer(dec_output)
         return final_output, attention_weights
 
     def forward(self, input_seq, target_seq, enc_padding_mask, dec_padding_mask, dec_target_padding_mask, look_ahead_mask):
         lowerdim_output, enc_output = self.encode(input_seq, enc_padding_mask)
-        final_output, attention_weights = self.decode(lowerdim_output, target_seq, dec_padding_mask, dec_target_padding_mask, look_ahead_mask)
+        # print(f"\tModel.forward: lowerdim_output.shape={lowerdim_output.shape}")
+        # print(f"\tModel.forward: enc_output.shape={enc_output.shape}")
+        
+        final_output, attention_weights = self.decode(
+            embedding=lowerdim_output,
+            target=target_seq,
+            dec_padding_mask=dec_padding_mask,
+            dec_target_padding_mask=dec_target_padding_mask,
+            look_ahead_mask=look_ahead_mask)
         return final_output, lowerdim_output #, enc_output, attention_weights
 
     # def encode_from_seq(self, inp_seq):

singleline_models/sketch_transformer/trainer.py
@@ -198,14 +198,24 @@ class Trainer():
 
         data = batch[0].to(self.device)
         
-        # hack: add 1 dimension
-        inp = torch.cat([data, data[:, -1, :].unsqueeze(1)], dim=1)
+        # # hack: add 1 dimension
+        # inp = torch.cat([data, data[:, -1, :].unsqueeze(1)], dim=1)
+        inp = data
         tar_inp = inp[:, :-1, ...]
         tar_real = inp[:, 1:, ...]
 
-        enc_padding_mask, dec_padding_mask, dec_target_padding_mask, look_ahead_mask = create_masks(tar_inp, tar_inp, device=self.device)
+        # print(f"inp.shape = {inp.shape}")
+        # print(f"tar_inp.shape = {tar_inp.shape}")
+        # print(f"tar_real.shape = {tar_real.shape}")
 
-        recon, _ = self.model(tar_inp, tar_inp, enc_padding_mask, dec_padding_mask, dec_target_padding_mask, look_ahead_mask)
+        enc_padding_mask, dec_padding_mask, dec_target_padding_mask, look_ahead_mask = create_masks(inp, tar_inp, device=self.device)
+        
+        # print(f"enc_padding_mask.shape = {enc_padding_mask.shape}")
+        # print(f"dec_padding_mask.shape = {dec_padding_mask.shape}")
+        # print(f"dec_target_padding_mask.shape = {dec_target_padding_mask.shape}")
+        # print(f"look_ahead_mask.shape = {look_ahead_mask.shape}")
+
+        recon, _ = self.model(inp, tar_inp, enc_padding_mask, dec_padding_mask, dec_target_padding_mask, look_ahead_mask)
 
         loss = self.loss(recon, tar_real)
 
@@ -222,14 +232,14 @@ class Trainer():
             
             # Optimize
             self.optimizer.step()
-        return loss.item(), data.shape[0]
+        return data.shape[0], loss.item()
 
     def validate_one_epoch(self, epoch):
         self.model.eval()
         total_items, total_loss = 0, 0
         with torch.no_grad():    
             for batch in iter(self.valid_loader):
-                loss, batch_items = self.step(batch, is_training=False)
+                batch_items, loss = self.step(batch, is_training=False)
 
                 total_loss += loss * batch_items
                 total_items += batch_items
@@ -245,23 +255,11 @@ class Trainer():
         for idx, batch in enumerate(progress_bar(iter(self.train_loader), parent=parent_progressbar)):
             self.scheduler.step()
             self.total_steps = idx + epoch * steps_per_epoch
-            loss, _ = self.step(batch, is_training=True)
+            _, loss = self.step(batch, is_training=True)
             self.log(dict(
                 loss=loss,
                 epoch=epoch,
                 learning_rate=self.optimizer.param_groups[0]['lr']))
-        
-    #     # update learning rate, if use_lr_decay is enabled
-    #     if self.hp.use_lr_decay:
-    #         if self.learning_rate > self.hp.min_lr:
-    #             self.learning_rate *= self.hp.lr_decay
-    #         self.optimizer = self.update_lr(self.optimizer, self.learning_rate)
-
-    # def update_lr(self, optimizer, lr):
-    #     """Decay learning rate by a factor of lr_decay"""
-    #     for param_group in optimizer.param_groups:
-    #         param_group['lr'] = lr
-    #     return optimizer
         
     def train(self):
         mb = master_bar(range(self.hp.epochs))